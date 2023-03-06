006dc618c37f16672e4878ee2c92aa4c4afa22c2
Taylor <taylor@roadrunnercharters.com>
2023-03-06 15:11:44 -0600

    fixed button link on manufacturers list

--- ghi/app/src/components/manufacturers/ManufacturersList.js
+++ ghi/app/src/components/manufacturers/ManufacturersList.js
@@ -35,7 +35,7 @@ const ManufacturersList = () => {
                     );
                 })}
             </tbody>
-            <Link to='/new' className="btn btn-primary">Create Manufacturer</Link>
+            <Link to='/manufacturers/new' className="btn btn-primary">Create Manufacturer</Link>
         </table>
     </>
 }
